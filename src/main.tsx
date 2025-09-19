b8c41fadac153c286c6e0b0e34b7eb1ef4300bbd
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-19 14:41:34 +0800

    feat: added theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,17 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+);
