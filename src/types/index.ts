3b0375676013a4fbe164b36bcb828100d6b8a80d
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-21 00:41:20 +0800

    feat: expanded schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,12 @@
 export type Book = {
-  author_key: string[];
-  author_name: string[];
+  author_key?: string[];
+  author_name?: string[];
+  authors?: { key: string; name: string }[];
   cover_edition_key?: string;
-  cover_i: number;
-  edition_count: number;
-  first_publish_year: number;
+  cover_i?: number;
+  cover_id?: number;
+  edition_count?: number;
+  first_publish_year?: number;
   key: string;
   title: string;
 };
