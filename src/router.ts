d6ca33e11b135541d098cf3424079ecf0e3c021b
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-20 12:59:24 +0800

    fix(refactor): fixed imports prior to renamed pages

--- src/router.ts
+++ src/router.ts
@@ -1,22 +1,24 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./Layout";
-import About from "./pages/About";
-import Browse from "./pages/Browse";
-import Home from "./pages/Home";
-import Random from "./pages/Random";
-import Trending from "./pages/Trending";
+import AboutPage from "./pages/AboutPage";
+import BrowsPage from "./pages/BrowsePage";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import RandomPage from "./pages/RandomPage";
+import TrendingPage from "./pages/TrendingPage";
 
 export const links = [
-  { path: "/", Component: Home, name: "Home" },
-  { path: "/trending", Component: Trending, name: "Trending" },
-  { path: "/browse", Component: Browse, name: "Browse" },
-  { path: "/random", Component: Random, name: "Random" },
-  { path: "/about", Component: About, name: "About" },
+  { path: "/", Component: HomePage, name: "Home" },
+  { path: "/trending", Component: TrendingPage, name: "Trending" },
+  { path: "/browse", Component: BrowsPage, name: "Browse" },
+  { path: "/random", Component: RandomPage, name: "Random" },
+  { path: "/about", Component: AboutPage, name: "About" },
 ];
 
 export const router = createBrowserRouter([
   {
     Component: Layout,
+    ErrorBoundary: ErrorPage,
     children: links.map(({ name, ...rest }) => rest),
   },
 ]);
