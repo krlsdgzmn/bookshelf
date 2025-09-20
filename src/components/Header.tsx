7666208008337c36ab96cb05d955f248373dae5b
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-20 13:00:59 +0800

    fix(refactor): refactored styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,6 +5,14 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { Link, useLocation } from "react-router";
 import { links } from "../router";
+import {
+  activeLinkStyle,
+  appBarStyles,
+  leftBoxStyle,
+  linkStyle,
+  titleStyle,
+  toolbarStyle,
+} from "../styles/headerStyles";
 import ToggleThemeButton from "./ToggleThemeButton";
 
 export default function Header() {
@@ -15,19 +23,20 @@ export default function Header() {
       <Toolbar sx={toolbarStyle}>
         <Box sx={leftBoxStyle}>
           <LibraryBooks />
-          <Link to="/" style={linkStyle}>
-            <Typography sx={titleStyle}>BOOKSHELF</Typography>
-          </Link>
+          <Typography component={Link} to={"/"} sx={titleStyle}>
+            BOOKSHELF
+          </Typography>
 
           {links.map((link) => (
-            <Link
+            <Typography
+              component={Link}
               to={link.path}
               style={
                 location.pathname === link.path ? activeLinkStyle : linkStyle
               }
             >
-              <Typography sx={linkTypographyStyle}>{link.name}</Typography>
-            </Link>
+              {link.name}
+            </Typography>
           ))}
         </Box>
 
@@ -36,24 +45,3 @@ export default function Header() {
     </AppBar>
   );
 }
-
-const appBarStyles = { paddingTop: 2, backgroundColor: "transparent" };
-const toolbarStyle = {
-  justifyContent: "space-between",
-  borderRadius: 8,
-  backgroundColor: "rgba(20, 20, 20, 0.8)",
-  backdropFilter: "blur(5px)",
-  WebkitBackdropFilter: "blur(5px)",
-  border: "1px solid rgba(255, 255, 255, 0.1)",
-};
-const leftBoxStyle = { display: "flex", gap: 4, alignItems: "center" };
-const titleStyle = { fontWeight: "bold", letterSpacing: 2, color: "white" };
-const linkStyle = {
-  textDecoration: "none",
-  borderBottom: "1.5px solid transparent",
-};
-const activeLinkStyle = {
-  textDecoration: "none",
-  borderBottom: "1.5px solid rgba(137, 180, 250, 1)",
-};
-const linkTypographyStyle = { color: "white" };
