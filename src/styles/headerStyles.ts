45269bcd64600e5c0dd24126a1cf17d88baf0cf3
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-20 17:42:58 +0800

    feat: added more styles for headers

--- src/styles/headerStyles.ts
+++ src/styles/headerStyles.ts
@@ -1,4 +1,5 @@
 export const appBarStyles = { paddingTop: 2, backgroundColor: "transparent" };
+
 export const toolbarStyle = {
   justifyContent: "space-between",
   borderRadius: 8,
@@ -7,20 +8,39 @@ export const toolbarStyle = {
   WebkitBackdropFilter: "blur(5px)",
   border: "1px solid rgba(255, 255, 255, 0.1)",
 };
-export const leftBoxStyle = { display: "flex", gap: 4, alignItems: "center" };
+
+export const leftBoxStyle = {
+  display: "flex",
+  gap: { xs: 1.5, md: 3 },
+  alignItems: "center",
+};
+
+export const mobileMenuLinksStyle = { display: { xs: "block", md: "none" } };
+
+const { display, ...restDesktopLinksStyle } = leftBoxStyle;
+export const desktopLinksStyle = {
+  display: { xs: "none", md: "flex" },
+  ...restDesktopLinksStyle,
+};
+
 export const titleStyle = {
+  fontSize: { xs: "small", md: "medium" },
   textDecoration: "none",
   fontWeight: "bold",
   letterSpacing: 2,
   color: "white",
+  paddingTop: 0.5,
 };
+
 export const linkStyle = {
+  padding: 4,
   color: "white",
   textDecoration: "none",
   borderBottom: "1.5px solid transparent",
 };
+
+const { borderBottom, ...restLinkStyle } = linkStyle;
 export const activeLinkStyle = {
-  color: "white",
-  textDecoration: "none",
-  borderBottom: "1.5px solid rgba(137, 180, 250, 1)",
+  ...restLinkStyle,
+  borderBottom: "2px solid rgba(137, 180, 250, 1)",
 };
