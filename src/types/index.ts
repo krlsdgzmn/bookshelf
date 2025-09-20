7c0bc71e93032f0d04d969a7a4153208da3680e0
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-20 21:42:13 +0800

    feat: added Book type

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type Book = {
+  author_key: string[];
+  author_name: string[];
+  cover_edition_key?: string;
+  cover_i: number;
+  edition_count: number;
+  first_publish_year: number;
+  key: string;
+  title: string;
+};
