a83354fda94b0920d9ca0a3baaf8bd41fa419b9d
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-21 02:53:11 +0800

    feat: implemented browse page

--- src/pages/BrowsePage.tsx
+++ src/pages/BrowsePage.tsx
@@ -1,3 +1,81 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import axios, { AxiosError } from "axios";
+import { useState } from "react";
+import FetchError from "../components/FetchError";
+import Section from "../components/Section";
+import { buttonStyle, pageBoxStyle } from "../styles/globalStyles";
+import {
+  headerStyle,
+  subtitleStyle,
+  titleStyle,
+} from "../styles/randomPageStyles";
+import type { Book } from "../types";
+
 export default function BrowsePage() {
-  return <div>Browse</div>;
+  const [query, setQuery] = useState("");
+  const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [hasSearched, setHasSearched] = useState(false);
+
+  async function handleSearch() {
+    if (!query.trim()) return;
+
+    setIsLoading(true);
+    setError("");
+    setHasSearched(true);
+
+    axios({
+      url: "https://openlibrary.org/search.json",
+      method: "GET",
+      params: { limit: 24, q: query },
+    })
+      .then((res) => setBooks(res.data.docs))
+      .catch((err: AxiosError) => setError(err.message))
+      .finally(() => setIsLoading(false));
+  }
+
+  if (!isLoading && error) return <FetchError error={error} />;
+
+  return (
+    <Box sx={pageBoxStyle}>
+      <Box sx={headerStyle}>
+        <Typography sx={titleStyle}>Uncover Your Next Favorite</Typography>
+
+        <Typography sx={subtitleStyle}>
+          Search for books by title, author, or keyword.
+        </Typography>
+
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <TextField
+            placeholder="Enter search query..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            onKeyPress={(e) => e.key === "Enter" && handleSearch()}
+            variant="standard"
+            sx={{ width: { xs: "200px", md: "250px" } }}
+          />
+          <Button variant="contained" onClick={handleSearch} sx={buttonStyle}>
+            Search
+          </Button>
+        </Box>
+      </Box>
+
+      <Section
+        title={books.length > 0 ? `Search Results for "${query}"` : ""}
+        books={books}
+        isLoading={isLoading}
+        isTrendingSection={false}
+      />
+
+      {!isLoading && hasSearched && books.length === 0 && !error && (
+        <Typography sx={{ textAlign: "center", mt: 4 }}>
+          No books found for "{query}". Try a different search.
+        </Typography>
+      )}
+    </Box>
+  );
 }
