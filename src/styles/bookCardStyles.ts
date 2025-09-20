af632c867d15bae11b94d97079dd9595dbd6dd62
krlsdgzmn <johnkarlos.deguzman.d@gmail.com>
2025-09-20 21:42:58 +0800

    feat: added more styles

---
+++ src/styles/bookCardStyles.ts
@@ -0,0 +1,32 @@
+export const cardStyle = {
+  backgroundColor: "rgba(20, 20, 20, 0.8)",
+  height: "325px",
+  width: "100%",
+};
+export const cardActionStyle = {
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "start",
+  justifyContent: "start",
+};
+export const titleStyle = {
+  fontSize: "1.25rem",
+  overflow: "hidden",
+  width: "100%",
+  WebkitLineClamp: 3,
+  WebkitBoxOrient: "vertical",
+  display: "-webkit-box",
+  color: "white",
+};
+export const authorStyle = { color: "#9C9C9C" };
+export const publishStyle = {
+  position: "absolute",
+  color: "#9C9C9C",
+  bottom: 14,
+};
+export const cardContentStyle = {
+  height: "100%",
+  width: "100%",
+  position: "relative",
+};
